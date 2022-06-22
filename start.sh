25d49f9910e78a2809c7182a27a38f10e7990c6e
Jade <jadecarreck@gmail.com>
2022-06-22 22:02:33 +0100

    Readme Updates

---
+++ start.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.10
+sudo apt install python3.10-venv
+python3.10 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3.10 app.py
