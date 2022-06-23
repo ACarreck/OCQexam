a9651b7d3a3e880961fe72c24a31d3eb5c74c22a
Jade <jadecarreck@gmail.com>
2022-06-23 10:33:31 +0100

    hotfix continuous operation

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ sudo apt install python3.10-venv
 python3.10 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python3.10 app.py
+nohup python3.10 app.py > log.text &
+tail log.text -f
